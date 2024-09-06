6d02d37be239df25531e41237cd55ca9b1f265db
Broderick <zerothesu@gmail.com>
2024-09-05 21:02:34 -0600

    added header and logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css'
+import kraken from './kracken.svg'
+import Header from './Header.js'
+
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>kracken</h1>
+      <img src={kraken} className='kraken-logo'/>
     </div>
   )
 }
